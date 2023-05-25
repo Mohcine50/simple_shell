bee754c6a3533106166121034d249ce3326a3ab5
shegami <mohcinela1998@gmail.com>
2023-05-25 10:58:52 -0700

    fix betty

--- shell.c
+++ shell.c
@@ -7,56 +7,21 @@
  */
 char *_getenv(const char *name)
 {
-    int i = 0;
-    char *splited_env;
-    char *env_value;
+	int i = 0;
+	char *splited_env;
+	char *env_value;
 
-    while (environ[i])
-    {
-        splited_env = strtok(environ[i], "=");
-        if (strcmp(splited_env, name) == 0)
-        {
-            env_value = strtok(NULL, "=");
-            return (env_value);
-        }
-        i++;
-    }
-    return (NULL);
-}
-
-/**
- * handle_command - check if the command on dir if not generate a command dir and check it
- * @com: command provided by the user
- * Return: pointer of the dir or NULL
- */
-char *handle_command(char *com)
-{
-    char *PATH;
-    char *command;
-    char *token;
-    struct stat _stat;
-
-    PATH = _getenv("PATH");
-    token = strtok(PATH, ":");
-
-    if (stat(com, &_stat) == 0)
-        return (com);
-    else
-        while (token)
-        {
-            command = malloc(strlen(token) + strlen(com) + 2);
-
-            strcpy(command, token);
-            strcat(command, "/");
-            strcat(command, com);
-            if (stat(command, &_stat) == 0)
-                return (command);
-
-            free(command);
-            token = strtok(NULL, ":");
-        }
-
-    return (NULL);
+	while (environ[i])
+	{
+		splited_env = strtok(environ[i], "=");
+		if (strcmp(splited_env, name) == 0)
+		{
+			env_value = strtok(NULL, "=");
+			return (env_value);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 /**
@@ -66,8 +31,8 @@ char *handle_command(char *com)
  */
 void print_shell(char *text, int length)
 {
-    if (isatty(STDIN_FILENO))
-        write(STDERR_FILENO, text, length);
+	if (isatty(STDIN_FILENO))
+		write(STDERR_FILENO, text, length);
 }
 
 /**
@@ -78,15 +43,15 @@ void print_shell(char *text, int length)
  */
 void shell_exit(char *command, char *input, char **argv)
 {
-    int num_exit = 0;
+	int num_exit = 0;
 
-    if (argv[1])
-        num_exit = _atoi(argv[1]);
+	if (argv[1])
+		num_exit = _atoi(argv[1]);
 
-    free(command);
-    free(input);
-    free(argv);
-    exit(num_exit);
+	free(command);
+	free(input);
+	free(argv);
+	exit(num_exit);
 }
 
 /**
@@ -95,12 +60,13 @@ void shell_exit(char *command, char *input, char **argv)
  */
 void _execve(char **argv)
 {
-    char *command = handle_command(argv[0]);
-    if (command)
-        execve(command, argv, environ);
-    else
-        perror("error");
+	char *command = handle_command(argv[0]);
 
-    free(argv);
-    exit(0);
+	if (command)
+		execve(command, argv, environ);
+	else
+		perror("error");
+
+	free(argv);
+	exit(0);
 }
